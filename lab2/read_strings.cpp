8fc4a95c2dbb8c1aee46058340995f30e8157d53
Vitaly <lildid32@mail.ru>
2024-11-13 19:23:50 +0500

    Create read-strings function

--- lab2/read_strings.cpp
+++ lab2/read_strings.cpp
@@ -5,5 +5,16 @@
 using namespace std;
 void read_data()
 {
+    setlocale(LC_ALL,"RU");
+    vector<int> stuff;
+    ifstream inf;
+    inf.open("file.txt");
+    while (!inf.eof())
+    {
+        int a;
+        inf >> a;;
+        stuff.push_back(a);
 
+    }
+    inf.close();
 }
