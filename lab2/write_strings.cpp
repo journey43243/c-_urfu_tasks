47b52b8a74c3685c7bec445a955e44e2fd23f17c
Ivan <piligrimovivan2@gmail.com>
2024-11-16 20:19:11 +0500

    second function

--- lab2/write_strings.cpp
+++ lab2/write_strings.cpp
@@ -1,5 +1,14 @@
-
-void write_strings()
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include "headers.h"
+using namespace std;
+void write_strings(vector<int> stuff)
 {
-
+    setlocale(LC_ALL,"RUSSIAN");
+    for (int i = 0; i < stuff.size(); i++)
+    {
+        int a = stuff[i];
+        cout << a;
+    }
 }
